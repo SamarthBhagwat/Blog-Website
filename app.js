54fca191d0220e0e99efc07fa2aef50662284628
Samarth <samarthrn19@gmail.com>
2021-07-27 15:29:41 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,80 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const homeStartingContent = "Hello there , This is a personal blog website made for writing blogs, notes, journal etc. It can be used as a dairy as well. Just click on the compose button to write a new blog or note. All your work will be saved so dont worry , just enjoy the website...";
+const aboutContent = "I am Samarth Bhagwat. I am an Information Technology student who is passionate about coding in general. I am currently persuing Bachelor of Engineering in Information Technology from Institute of Engineering and Technology , Devi Ahilya Vishwavidyalaya, Indore. I am currently in my final year. I love making website. I am really passionate about web development. Other than development, I also solve problems of Data Structures and Algorithms on various platforms like GFG, Leetcode etc. Other than a coder, I am a pianist as well. I have been a lead pianist in the Official band of our college called as 'Goonj'. I love music. If you like music as much as I do , do contact me to discuss about music and coding , we can also code together while listening to music. Thanks. Enjoy your day.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function(req, res){
+
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
+  });
+});
+
+app.get("/posts/:postId", function(req, res){
+
+const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
